571d8c2de4cacce018c89d0170f234a444995918
DevonFulcher <devonfulcher3@gmail.com>
2019-04-20 16:02:04 -0500

    separating input/output and train/test

--- tasks/crypto_predictor/utils.py
+++ tasks/crypto_predictor/utils.py
@@ -1,24 +1,7 @@
 import csv
 import numpy as np
-import tensorflow as tf
-from tensorflow.python.framework import ops
-'''
-ops.reset_default_graph()
+import math
 
-sess = tf.Session()
-list_tensor = []
-
-with open('bitcoin prices 4-3-18 to 4-3-19.csv', newline='') as csvfile:
-    data = csv.reader(csvfile)
-    for row in data:
-        print(row[0], row[1])
-        list_tensor.append(row)
-
-something = tf.constant(list_tensor)
-print(something)
-
-# for data in list_tensor:
-#print((data[0],data[1]))'''
 
 def data_generator():
     #this is a constant for now
@@ -29,23 +12,27 @@ def data_generator():
 
     with open('bitcoin prices 4-3-18 to 4-3-19.csv', newline='') as csvfile:
         data = csv.reader(csvfile)
-        training_set = data[:portion_training_set * len(data)]
-        test_set = data[len(data) - portion_training_set * len(data):]
-        (x_train, y_train) = generate_x_and_y(training_set, length_of_convolution, portion_training_set)
-        (x_test, y_test) = generate_x_and_y(test_set, length_of_convolution, portion_training_set)
+        prices = []
+        for datum in data:
+            prices.append(datum[1])
+        train_set_size = math.floor(portion_training_set * len(prices))
+        training_set = prices[:train_set_size]
+        test_set = prices[train_set_size:]
+        (x_train, y_train) = generate_x_and_y(training_set, length_of_convolution)
+        (x_test, y_test) = generate_x_and_y(test_set, length_of_convolution)
 
     return (x_train, y_train), (x_test, y_test)
 
 
-def generate_x_and_y(observations_set, length_of_convolution, portion_training_set):
+def generate_x_and_y(observations_set, length_of_convolution):
     x = np.array([])
     y = np.array([])
-    for j in range(len(observations_set) - length_of_convolution - 1):
-        i = 0
+    for i in range(len(observations_set) - length_of_convolution - 1):
+        j = 0
         for _ in range(length_of_convolution):
-            x.append(observations_set[j + i][1])
-            i += 1
-        y.append(observations_set[j + i + 1][1])
+            x = np.append(x, observations_set[i + j])
+            j += 1
+        y = np.append(y, observations_set[i + j + 1])
     return x, y
 
 if __name__ == '__main__':
